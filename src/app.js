aaf9b052eca045c246ab2943b686e96e0dfa1163
subhojeetprasad0 <subhojeetprasad0@gmail.com>
2026-03-08 16:31:01 +0530

    add project stuture, auth.js in lirary functions, and configured pool for db connection, added jsonwebtokens and created the jwt sign function to siign a user but ran into an error

--- src/app.js
+++ src/app.js
@@ -1,8 +1,52 @@
 import express from 'express';
+import query from './config/db.js'
+import generateToken from './lib/auth.js'
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); 
 
 app.get("/", (req, res) => {
   res.send("Server working");
+});
+
+app.post("/login",async(req,res)=>{
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json(
+        { message: "Email and password required" }
+      );
+    }
+
+    const result = await query(
+      "SELECT * FROM users WHERE email = $1",
+      [email]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(401).json(
+        { message: "Invalid credentials" }
+      );
+    }
+
+    const user = result.rows[0];
+
+    const isMatch = (password===user.password);
+
+    if (!isMatch) {
+      return res.status(401).json(
+        { message: "Invalid credentials" }
+      );
+    }
+
+    const token = generateToken(user);
+    res.json({token:token});
+  }catch(err){
+    console.log(`LOGIN ERROR: ${err}`);
+  }
+
 });
 
 export default app;
