8f2769fc3f1a6684d9c2a60976d52cf77865ac31
Prasad1-S <subhojeetprasad123@gmail.com>
2026-04-06 21:10:01 +0530

    add: verifying the token type while authentication

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,6 +4,9 @@ export default function authenticate(req, res, next){
     const token = req.cookies.session;
     if(!token) return res.status(401).json({Error:"Not Logged in!"});
 
+    req.user = verifyToken(token);
+    console.log(req.user);
+    if(req.user.type!="session") return res.status(401).json({Error:"Invalid or Expired Token!"})
     try {
         req.user = verifyToken(token);
         next();
